e2d6a585e30bd7f12ff6127969038703e4f58ecc
ntd-97 <you@example.comntdat190597@gmail.com>
2022-10-19 23:26:53 +0700

    add padding to errorpage

--- src/components/common/ErrorPage.js
+++ src/components/common/ErrorPage.js
@@ -4,7 +4,7 @@ import { TiWarning } from "react-icons/ti";
 
 const ErrorPage = ({ msg }) => {
   return (
-    <div className="PageNotFound flex h-screen items-center justify-center text-center text-2xl text-primary lg:text-4xl 2xl:text-5xl">
+    <div className="PageNotFound flex h-screen items-center justify-center px-4 text-center text-2xl text-primary lg:text-4xl 2xl:text-5xl">
       <p>
         <TiWarning className="mr-2 inline-block text-3xl lg:text-5xl 2xl:text-6xl" />
         {msg}
