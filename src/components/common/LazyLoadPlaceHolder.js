9585ad45e9d45577c03a8b9cbffbec49e1954e6f
ntd-97 <you@example.comntdat190597@gmail.com>
2022-10-23 23:37:20 +0700

    test rounded netlify

--- src/components/common/LazyLoadPlaceHolder.js
+++ src/components/common/LazyLoadPlaceHolder.js
@@ -3,7 +3,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const LazyLoadPlaceHolder = ({ imgLoaded, children, rounded }) => {
-  const placeHolderClass = `before:absolute before:${rounded} before:inset-0 before:z-[49] before:animate-pulse before:bg-[#181818] before:content-['']`;
+  const placeHolderClass = `before:absolute before:rounded-[10px] before:inset-0 before:z-[49] before:animate-pulse before:bg-[#181818] before:content-['']`;
   return (
     <div className={`${imgLoaded ? placeHolderClass : ""} relative w-full`}>
       {children}
