78ab83e4443470d2fe0d040ca51f9fd14dfb3579
ntd-97 <you@example.comntdat190597@gmail.com>
2022-10-24 14:47:02 +0700

    split searchSideBar

--- src/components/SearchSideBar/SearchSideBar.js
+++ src/components/SearchSideBar/SearchSideBar.js
@@ -1,38 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 
-import { FiSearch } from "react-icons/fi";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-
-import { useLocation, useNavigate } from "react-router-dom";
-
-import useDebounce from "../../hooks/useDebounce";
+import { useNavigate } from "react-router-dom";
 
 import SearchSideBarList from "./SearchSideBarList";
+import SearchSideBarInput from "./SearchSideBarInput";
 
 import { useSelector } from "react-redux";
 
 const SearchSideBar = () => {
   const [lists, setLists] = useState({});
 
-  const [type, setType] = useState({});
-
-  const [searchQuery, setSearchQuery] = useState("");
-
   const [openSearchSidebar, setOpenSearchSidebar] = useState(false);
-
-  const typeRef = useRef("");
 
   const loginInfo = useSelector((state) => state.loginInfo);
 
-  // preventing call API a lots of times when the user types in the search input
-  const debouncedSearchQuery = useDebounce(searchQuery, 500);
-
-  const { pathname } = useLocation();
   const navigate = useRef(useNavigate());
-
-  const clickOpenSideBarHandler = () => {
-    setOpenSearchSidebar(!openSearchSidebar);
-  };
 
   useEffect(() => {
     const getLists = async () => {
@@ -98,50 +80,6 @@ const SearchSideBar = () => {
     getLists();
   }, [loginInfo]);
 
-  useEffect(() => {
-    // set search placeholder and path to navigate base on the pathname
-    if (pathname.includes("movies")) {
-      setType({
-        pathSearch: "movies",
-        name: "Movies",
-      });
-
-      typeRef.current = "movies";
-    }
-
-    if (pathname.includes("tvseries")) {
-      setType({
-        pathSearch: "tvseries",
-        name: "TV Series",
-      });
-
-      typeRef.current = "tvseries";
-    }
-
-    if (pathname.includes("celebs")) {
-      setType({
-        pathSearch: "celebs",
-        name: "Celebs",
-      });
-
-      typeRef.current = "celebs";
-    }
-
-    if (!pathname.includes("search")) {
-      setSearchQuery("");
-    }
-  }, [pathname]);
-
-  useEffect(() => {
-    // navigation when the user types in the search input
-    if (debouncedSearchQuery) {
-      setOpenSearchSidebar(false);
-      navigate.current(
-        `/${typeRef.current}/search/page/1?query=${debouncedSearchQuery}`
-      );
-    }
-  }, [debouncedSearchQuery]);
-
   return (
     <div
       className={`${
@@ -150,33 +88,10 @@ const SearchSideBar = () => {
           : "h-[68px] overflow-hidden"
       } no-scrollbar fixed bottom-0 left-0 right-0 z-[100] flex w-full flex-col bg-[#181818] pb-4 text-[#ececec] transition-all duration-200 ease-in lg:relative  lg:z-auto  lg:h-screen lg:w-[30%] lg:overflow-scroll lg:border-l-2 lg:border-[#353535] lg:pb-5 xl:w-[40%] 2xl:w-[35%]`}
     >
-      <div className="sticky top-0 z-50 flex gap-x-2 bg-[#181818] bg-opacity-95 py-3 px-3 lg:py-5 xl:px-6">
-        {!openSearchSidebar && (
-          <IoIosArrowUp
-            onClick={clickOpenSideBarHandler}
-            className="rounded-[10px] bg-[#252229] p-2 text-[44px] lg:hidden"
-          />
-        )}
-
-        {openSearchSidebar && (
-          <IoIosArrowDown
-            onClick={clickOpenSideBarHandler}
-            className="rounded-[10px] bg-[#252229] p-2 text-[44px] lg:hidden"
-          />
-        )}
-
-        <FiSearch className="absolute top-1/2 right-[25px] -translate-y-1/2 text-[22px] text-[#9CA3AF] lg:right-[39px] lg:hidden xl:block" />
-
-        <input
-          type="text"
-          placeholder={`Search ${type.name}`}
-          className="w-full truncate rounded-[10px] bg-[#252229] px-[15px] py-[10px] outline-none focus:outline-[#353535]"
-          onChange={(e) => {
-            setSearchQuery(e.target.value);
-          }}
-          value={searchQuery}
-        />
-      </div>
+      <SearchSideBarInput
+        openSearchSidebar={openSearchSidebar}
+        setOpenSearchSidebar={setOpenSearchSidebar}
+      ></SearchSideBarInput>
 
       <div className="mt-5 grid gap-y-10 px-3 xl:px-6">
         {lists?.trending?.map((list) => (
